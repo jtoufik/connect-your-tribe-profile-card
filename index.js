3799c6ff91e93efda81c7719f0241cf10ae2da38
jtoufik <112856590+jtoufik@users.noreply.github.com>
2023-02-08 11:30:06 +0100

    Json connectie gemaakt

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
+const url = 'https://whois.fdnd.nl/api/v1/member/m-jalal'
+const data = await fetch(url).then((response) => response.json())
+
+console.log (data)
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,7 +19,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
